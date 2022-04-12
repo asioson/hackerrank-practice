0efffa51b033988df502fde7dc673c56d64ba54d
asioson <allan.sioson@gmail.com>
2022-04-12 11:49:18 +0800

    first commit

---
+++ 30-days-of-code/11-2d-arrays/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    var arr [][]int32
+    for i := 0; i < 6; i++ {
+        arrRowTemp := strings.Split(strings.TrimRight(readLine(reader)," \t\r\n"), " ")
+
+        var arrRow []int32
+        for _, arrRowItem := range arrRowTemp {
+            arrItemTemp, err := strconv.ParseInt(arrRowItem, 10, 64)
+            checkError(err)
+            arrItem := int32(arrItemTemp)
+            arrRow = append(arrRow, arrItem)
+        }
+
+        if len(arrRow) != 6 {
+            panic("Bad input")
+        }
+
+        arr = append(arr, arrRow)
+    }
+    maxScore := int32(-9 * 7)
+    for j := 0; j < 4; j++ {
+        for i := 0; i < 4; i++ {
+            score := arr[j+1][i+1]
+            for r := 0; r < 3; r++ {
+                score += arr[j][i+r] + arr[j+2][i+r]
+            }
+            if score > maxScore {
+                maxScore = score
+            }
+        }
+    }
+    fmt.Println(maxScore)
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+
