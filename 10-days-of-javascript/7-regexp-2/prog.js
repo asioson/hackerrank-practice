8111b1f68cd68bb63bf2ac6a7d482167a6bccd80
asioson <allan.sioson@gmail.com>
2022-04-13 22:46:41 +0800

    first commit

---
+++ 10-days-of-javascript/7-regexp-2/prog.js
@@ -0,0 +1,40 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function regexVar() {
+    /*
+     * match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', 
+     * followed by one or more letters.
+     */
+    let re = /^(Mr|Mrs|Ms|Dr|Er)\.[A-Za-z]+$/;
+    return re;
+}
+
+
+function main() {
+    const re = regexVar();
+    const s = readLine();
+    
+    console.log(!!s.match(re));
+}
