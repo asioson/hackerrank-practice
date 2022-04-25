0c8b4a81369d307d8304d9dff054097997ea5044
asioson <allan.sioson@gmail.com>
2022-04-25 14:07:58 +0800

    first commit

---
+++ 30-days-of-code/24-linked-list-deletion/prog.cpp
@@ -0,0 +1,106 @@
+#include <cstddef>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;	
+class Node
+{
+    public:
+        int data;
+        Node *next;
+        Node(int d){
+            data=d;
+            next=NULL;
+        }
+};
+class Solution{
+    public:
+
+          Node* removeDuplicates(Node *head)
+          {
+              if (head == NULL) return NULL;
+              Node *p = head;
+              Node *q = head->next;
+              while (q != NULL) {
+                  if (p->data == q->data) {
+                      Node *r = p;
+                      p = q;
+                      q = q->next;
+                      free(r);
+                  } else {
+                    break;
+                  }
+              }
+              head = p;
+              Node *t = p;
+              p = p->next;
+              while (p != NULL) {
+                  q = p->next;
+                  while (q != NULL) {
+                      if (p->data == q->data) {
+                          Node *r = p;
+                          p = q;
+                          q = q->next;
+                          free(r);
+                      } else {
+                         break;
+                      }
+                  }
+                  t->next = p;
+                  t = p;
+                  p = p->next;
+              }
+              return head;
+          }
+
+          Node* insert(Node *head,int data)
+          {
+               Node* p=new Node(data);
+               if(head==NULL){
+                   head=p;  
+
+               }
+               else if(head->next==NULL){
+                   head->next=p;
+
+               }
+               else{
+                   Node *start=head;
+                   while(start->next!=NULL){
+                       start=start->next;
+                   }
+                   start->next=p;   
+
+               }
+                    return head;
+                
+            
+          }
+          void display(Node *head)
+          {
+                  Node *start=head;
+                    while(start)
+                    {
+                        cout<<start->data<<" ";
+                        start=start->next;
+                    }
+           }
+};
+			
+int main()
+{
+	Node* head=NULL;
+  	Solution mylist;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        head=mylist.insert(head,data);
+    }	
+    head=mylist.removeDuplicates(head);
+
+	mylist.display(head);
+		
+}
